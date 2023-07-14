37a573b9a0ab42affd27f3b783d5c09f5b59f02f
Austin Nelson <therefectcorporation@gmail.com>
2023-07-14 14:35:41 -0400

    add preferences

--- lib/owl.dart
+++ lib/owl.dart
@@ -5,6 +5,7 @@ export 'animations/implicit/animated_number.dart';
 export 'misc/logger/ansi_color.dart';
 export 'misc/logger/log_error_handler.dart';
 export 'misc/logger/logger.dart';
+export 'misc/preferences.dart';
 export 'widgets/async/async_value_builder.dart';
 export 'widgets/async/item_list_builder.dart';
 export 'widgets/clickable.dart';
