7ab4499c8cdb4a9ba9c2fff73a4135679c949c52
Austin Nelson <therefectcorporation@gmail.com>
2023-07-14 14:29:37 -0400

    Add exports for logger and async widgets in owl.dart

--- lib/owl.dart
+++ lib/owl.dart
@@ -2,6 +2,11 @@ library owl;
 
 // export all public apis
 export 'animations/implicit/animated_number.dart';
+export 'misc/logger/ansi_color.dart';
+export 'misc/logger/log_error_handler.dart';
+export 'misc/logger/logger.dart';
+export 'widgets/async/async_value_builder.dart';
+export 'widgets/async/item_list_builder.dart';
 export 'widgets/clickable.dart';
 export 'widgets/conditional_parent_widget.dart';
 export 'widgets/responsive/responsive_center.dart';
